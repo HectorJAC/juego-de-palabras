9c8f8787c0457b0ba30af6dcade6ef4ca6cc0f2d
Hector <hjac10@hotmail.com>
2024-08-20 22:00:09 -0400

    commit 2

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './App.css';
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
